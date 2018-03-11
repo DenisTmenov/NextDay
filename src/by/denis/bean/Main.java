815364137c1eeafdb97b3a02ea90aa6fa169f928
Denis <denis.tmenov@gmail.com>
2018-03-12 00:40:15 +0300

    fix bug

--- src/by/denis/bean/Main.java
+++ src/by/denis/bean/Main.java
@@ -5,7 +5,7 @@ import by.denis.console.Calendar;
 public class Main {
 
     public static void main(String[] args) {
-        Calendar myBirthDay = new Calendar(213123, 44, 1988);
+        Calendar myBirthDay = new Calendar(27  , 4, 1988);
         myBirthDay.printFullDate();
         myBirthDay.plusDay();
         myBirthDay.printFullDate();
--- src/by/denis/console/Number.java
+++ src/by/denis/console/Number.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 class Number {
     private static Scanner scanner = new Scanner(System.in);
 
-
     static int getInt() {
         String word;
         do {
